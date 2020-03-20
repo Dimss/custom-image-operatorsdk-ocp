6c41b71f879ccb0e8b0d9e00281a7a08aec86b55
Dmitry Kartsev <dkartsev@redhat.com>
2020-03-20 18:46:55 +0200

    update build

--- build.sh
+++ build.sh
@@ -10,4 +10,7 @@ SRC_DIR=$(pwd)/src
 
 # Clone the source code
 git clone ${SOURCE_URI} ${SRC_DIR}
+cd ${SRC_DIR}
+sleep 3600
+operator-sdk build --image-builder buildah ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
 sleep 3600
