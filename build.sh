9150dd225dd5b08a053b349f7f13ded23ae5aca1
Dmitry Kartsev <dkartsev@redhat.com>
2020-03-20 20:22:39 +0200

    add push

--- build.sh
+++ build.sh
@@ -11,5 +11,12 @@ SRC_DIR=$(pwd)/src
 # Clone the source code
 git clone ${SOURCE_URI} ${SRC_DIR}
 cd ${SRC_DIR}
+# Build Ansible Operator image
 operator-sdk build --image-builder buildah ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
-sleep 3600
+# buildah requires a slight modification to the push secret provided by the service
+# account in order to use it for pushing the image
+# https://docs.openshift.com/container-platform/4.3/builds/custom-builds-buildah.html
+cp /var/run/secrets/openshift.io/push/.dockercfg /tmp
+(echo "{ \"auths\": " ; cat /var/run/secrets/openshift.io/push/.dockercfg ; echo "}") > /tmp/.dockercfg
+
+buildah  push --tls-verify=false --authfile /tmp/.dockercfg ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
